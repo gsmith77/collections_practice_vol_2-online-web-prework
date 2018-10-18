fc43e7919d64b3530e408368ed0720c1db406f18
grant smith <grantsmith1256@gmail.com>
2018-10-18 04:52:27 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,26 @@
-# your code goes here
+def begins_with_r(name)
+  name.map do |name|
+    if !name.start_with?("r")
+      return false
+end
+end
+return true
+end
+
+def contain_a (array)
+ array.select do |name|
+   name.include? ("a")
+ end
+ end
+ 
+ def first_wa(array)
+   array.find do |name|
+     name.to_s.start_with?("wa")
+   end
+end
+
+def remove_non_strings(array)
+  array.pop
+  array.pop
+  return array
+end
